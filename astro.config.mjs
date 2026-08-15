32ef78e7767bb0e2e4b9751457e7bfa92cad5226
root <root@PSYCHO-PC.localdomain>
2026-08-15 10:54:49 +0200

    Add github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://bluebu-dev.github.io',
+  base: '/lamplbruderschaft',
+
+  vite: {
+    server: {
+      watch: {
+        usePolling: true,
+      },
+    },
+  },
+});
